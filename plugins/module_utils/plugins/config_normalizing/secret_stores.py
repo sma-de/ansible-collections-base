2ef13f39ca1ab65838d665f185212e33473994d0
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2025-02-17 18:01:19 +0100

    wip

--- plugins/module_utils/plugins/config_normalizing/secret_stores.py
+++ plugins/module_utils/plugins/config_normalizing/secret_stores.py
@@ -2,6 +2,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import collections
 
 from ansible.errors import AnsibleOptionsError
 from ansible.module_utils.six import iteritems
