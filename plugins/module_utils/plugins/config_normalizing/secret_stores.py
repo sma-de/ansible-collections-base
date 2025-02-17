cffb00434ec9f535c0eeab6e7f211c8681ad0ad7
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2025-02-17 21:48:35 +0100

    wip

--- plugins/module_utils/plugins/config_normalizing/secret_stores.py
+++ plugins/module_utils/plugins/config_normalizing/secret_stores.py
@@ -59,10 +59,6 @@ class CredentialSettingsNormerBase(NormalizerBase):
         return True
 
     @property
-    def enable_default_stores(self):
-        return False
-
-    @property
     def stores_mandatory(self):
         return False
 
