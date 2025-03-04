3c60d9a7b59cadbc8b04a48fb90bba8d3608a7f6
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2025-03-04 09:23:32 +0100

    feat(roles/apt-sources): improve handling of ...
    ... signed sources by supporting de-armoring and getting key from
    fingerprints via keyservers

--- plugins/action/normalize_custom_apt_sources_args.py
+++ plugins/action/normalize_custom_apt_sources_args.py
@@ -3,6 +3,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
+import copy
 import pathlib
 
 from ansible.errors import AnsibleOptionsError
@@ -99,6 +100,9 @@ class SourcesInstNormer(NormalizerNamed):
         c = my_subcfg['config']
         c['repo'] = sl
 
+        if my_subcfg['apt_file']:
+            c['filename'] = my_subcfg['apt_file']
+
         setdefault_none(c, 'state', 'present')
 
         pcfg = self.get_parentcfg(cfg, cfgpath_abs, level=2)
@@ -117,6 +121,15 @@ class SourcesInstSigningNormer(NormalizerBase):
           'config', DefaultSetterConstant({})
         )
 
+        self._add_defaultsetter(kwargs,
+          'de_armor', DefaultSetterConstant(False)
+        )
+
+        subnorms = kwargs.setdefault('sub_normalizers', [])
+        subnorms += [
+          SourcesInstSignFingerPrintsNormer(pluginref),
+        ]
+
         super(SourcesInstSigningNormer, self).__init__(pluginref, *args, **kwargs)
 
 
@@ -127,6 +140,7 @@ class SourcesInstSigningNormer(NormalizerBase):
     @property
     def simpleform_key(self):
         return 'src_url'
+
 
     def _handle_specifics_presub(self, cfg, my_subcfg, cfgpath_abs):
         dest = my_subcfg.get('dest', None)
@@ -141,12 +155,142 @@ class SourcesInstSigningNormer(NormalizerBase):
 
         dest = str(dest)
         my_subcfg['dest'] = dest
+        return my_subcfg
+
+
+    def _handle_specifics_postsub(self, cfg, my_subcfg, cfgpath_abs):
+        src_url = my_subcfg.get('src_url', None)
+        fprints = my_subcfg['fingerprints']['fingerprints']
+
+        dest = my_subcfg['dest']
+
+        if fprints:
+            dest = dest.split('.')
+
+            ##
+            ## note: file ending is very important here and magic!
+            ##   if you have a keyring in gpg keybox format which
+            ##   you will always have when importing keys from
+            ##   fingerprints the filename must end with '.gpg'
+            ##   or it will not be handled correctly by apt and
+            ##   you will get "NO_PUBKEY" errors although the
+            ##   file contains the needed keys!!!
+            ##
+            if dest[-1] != 'gpg':
+                if len(dest) > 1:
+                    dest[-1] = 'gpg'
+                else:
+                    dest.append('gpg')
+
+            dest = '.'.join(dest)
+            my_subcfg['dest'] = dest
 
         c = my_subcfg['config']
-        c['url'] = my_subcfg['src_url']
         c['dest'] = dest
 
+        if src_url:
+            c['url'] = src_url
+
         return my_subcfg
+
+
+
+class SourcesInstSignFingerPrintsNormer(NormalizerBase):
+
+    def __init__(self, pluginref, *args, **kwargs):
+        subnorms = kwargs.setdefault('sub_normalizers', [])
+        subnorms += [
+          FingerPrintConfigDefaultsNormer(pluginref),
+          SourcesInstSignFingerPrintInstNormer(pluginref),
+        ]
+
+        super(SourcesInstSignFingerPrintsNormer, self).__init__(pluginref, *args, **kwargs)
+
+    @property
+    def config_path(self):
+        return ['fingerprints']
+
+    @property
+    def simpleform_key(self):
+        return '_fingerprint'
+
+    def _handle_specifics_presub(self, cfg, my_subcfg, cfgpath_abs):
+        sf_print = my_subcfg.pop(self.simpleform_key, None)
+
+        if sf_print:
+            sm = setdefault_none(my_subcfg, 'fingerprints', {})
+            sm[sf_print] = None
+
+        return my_subcfg
+
+
+    def _handle_specifics_postsub(self, cfg, my_subcfg, cfgpath_abs):
+        cfg_defs = my_subcfg['config_defaults']
+
+        exp_cfgs = []
+        exp_cfg_def = {
+          'key_ids': [],
+          'config': cfg_defs,
+        }
+
+        for k, v in my_subcfg['fingerprints'].items():
+            vc = v['config']
+
+            if not vc:
+                ## no custom settings for this fp, uses default config
+                exp_cfg_def['key_ids'].append(v['fingerprint'])
+                continue
+
+            ## this fingerprint has custom export settings,
+            ## make it its own command
+            vc = merge_dicts(copy.deepcopy(cfg_defs), vc)
+
+            exp_cfgs.append({
+              'key_ids': [v['fingerprint']],
+              'config': vc,
+            })
+
+        if exp_cfg_def['key_ids']:
+            exp_cfgs.append(exp_cfg_def)
+
+        if exp_cfgs:
+            my_subcfg['_export_cfgs'] = exp_cfgs
+
+        return my_subcfg
+
+
+
+class FingerPrintConfigDefaultsNormer(NormalizerBase):
+
+    def __init__(self, pluginref, *args, **kwargs):
+        self._add_defaultsetter(kwargs,
+          'keyserver', DefaultSetterConstant('hkp://keyserver.ubuntu.com:80')
+        )
+
+        super(FingerPrintConfigDefaultsNormer, self).__init__(pluginref, *args, **kwargs)
+
+    @property
+    def config_path(self):
+        return ['config_defaults']
+
+
+
+class SourcesInstSignFingerPrintInstNormer(NormalizerNamed):
+
+    def __init__(self, pluginref, *args, **kwargs):
+        self._add_defaultsetter(kwargs,
+          'config', DefaultSetterConstant({})
+        )
+
+        super(SourcesInstSignFingerPrintInstNormer, self).__init__(pluginref, *args, **kwargs)
+
+    @property
+    def config_path(self):
+        return ['fingerprints', SUBDICT_METAKEY_ANY]
+
+    @property
+    def name_key(self):
+        return 'fingerprint'
 
 
 
